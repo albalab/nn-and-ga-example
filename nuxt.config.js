bb8455ca897822361d4e93fe8305d1d589a42437
Albert <alb.bolush@gmail.com>
2021-02-23 21:21:35 +0100

    repository name added

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/<repository-name>/',
+    base: '/nn-and-ga-example/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
